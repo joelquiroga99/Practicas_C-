e5c84fd0f9012376a67869f69d745128106ca09b
joelquiroga99 <155786194+joelquiroga99@users.noreply.github.com>
2024-01-24 21:55:01 -0400

    Menu_Funciones.cpp

--- Menu_Funciones.cpp
+++ Menu_Funciones.cpp
@@ -4,30 +4,89 @@
 
 using namespace std;
 
-// Función para ingresar dos números desde el usuario
 void ingresar(int &numero1, int &numero2)
 {
-    system("cls"); // Limpia la pantalla
-    cout << "Ingrese un numero: ";
-    cin >> numero1; // Obtiene el primer número
-    cout << "Ingrese otro numero: ";
-    cin >> numero2; // Obtiene el segundo número
+	system("cls");
+	cout << "Ingrese un numero: ";
+	cin >> numero1;
+	cout << "Ingrese otro numero: ";
+	cin >> numero2;
 }
 
-// Función para sumar dos números
 void sumar(int numero1, int numero2, int &resultado)
 {
-    resultado = numero1 + numero2; // Calcula la suma y la almacena en 'resultado'
+	resultado = numero1 + numero2;
 }
 
-// Función para verificar si un número es primo
 bool es_primo(int numero)
 {
-    if (numero <= 1)
-        return false; // Los números menores o iguales a 1 no son primos
+	if (numero <= 1)
+		return false;
+	
+	for (int i = 2; i <= sqrt(numero); ++i)
+	{
+		if (numero % i == 0)
+			return false;
+		Sleep(2000);
+	}
+	return true;
+}
 
-    for (int i = 2; i <= sqrt(numero); ++i)
-    {
-        if (numero % i == 0)
-            return false; // Si es divisible por algún número entre 2 y la raíz cuadrada, no es primo
-        Sleep(2000); // Hace 
+void mostrar_menu(int &opcion)
+{
+	system("cls");
+	cout << "MENU" << endl;
+	cout << "1. Ingresar numeros" << endl;
+	cout << "2. Sumar numeros" << endl;
+	cout << "3. Verificar si un numero es primo" << endl;
+	cout << "0. Salir" << endl;
+	cout << "Ingrese su opcion: ";
+	cin >> opcion;
+}
+
+int main()
+{
+	int opcion, numero1, numero2, resultado;
+	
+	do
+	{
+		mostrar_menu(opcion);
+		
+		switch (opcion)
+		{
+		case 1:
+			ingresar(numero1, numero2);
+			break;
+		case 2:
+			system("cls");
+			sumar(numero1, numero2, resultado);
+			cout << "El resultado de la suma es: " << resultado << endl;
+			Sleep(2000);
+			break;
+		case 3:
+			system("cls");
+			cout << "Ingrese un numero para verificar si es primo: ";
+			cin >> numero1;
+			if (es_primo(numero1))
+			{
+				cout << numero1 << " es primo." << endl;
+			}
+			else
+			{
+				cout << numero1 << " no es primo." << endl;
+			}
+			Sleep(2000);
+			break;
+		case 0:
+			system("cls");
+			cout << "Saliendo del programa." << endl;
+			break;
+		default:
+			system("cls");
+			cout << "Opcion no valida. Intentelo de nuevo." << endl;
+			Sleep(2000);
+		}
+	} while (opcion != 0);
+	
+	return 0;
+}
