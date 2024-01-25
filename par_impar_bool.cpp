165e075cd7179b71b269cfafabff71cda0fe654e
joelquiroga99 <155786194+joelquiroga99@users.noreply.github.com>
2024-01-24 20:09:47 -0400

    par_impar_bool.cpp

--- par_impar_bool.cpp
+++ par_impar_bool.cpp
@@ -1,35 +1,63 @@
 #include <iostream>
 using namespace std;
 
-// Declaración de variable global
-int entero_num;
+// Declaracion de variables globales
+int n1, n2, num;
 
-// Declaración de función para determinar si un número es par o impar
-bool par_impar()
+// Declaracion de funcion para determinar si un número es par o impar
+bool par_impar(int num)
 {
-	return entero_num % 2 == 0;
+	return num % 2 == 0;
 }
 
-// Declaración de función para ingresar un número
+// Declaracion de funcion para ingresar dos numeros
 void ingresar()
 {
-	cout << "Ingresar un numero:";
-	cin >> entero_num;
+	cout << "Ingresar el primer numero:";
+	cin >> n1;
+	cout << "Ingresar el segundo numero:";
+	cin >> n2;
+}
+
+// Declaración de funcion para sumar dos números
+int sumar()
+{
+	return n1 + n2;
+}
+
+// Declaración de funcion para multiplicar dos números
+int multiplicar()
+{
+	return n1 * n2;
 }
 
 int main()
 {
 	ingresar();
 	
-	// Determinar si el número ingresado es par o impar
-	if (par_impar())
+	// Determinar si el primer numero ingresado es par o impar
+	if (par_impar(n1))
 	{
-		cout << "El numero ingresado es par." << endl;
+		cout << "El primer numero ingresado es par." << endl;
 	}
 	else
 	{
-		cout << "El numero ingresado es impar." << endl;
+		cout << "El primer numero ingresado es impar." << endl;
 	}
+	
+	// Determinar si el segundo numero ingresado es par o impar
+	if (par_impar(n2))
+	{
+		cout << "El segundo numero ingresado es par." << endl;
+	}
+	else
+	{
+		cout << "El segundo numero ingresado es impar." << endl;
+	}
+	
+	// Mostrar la suma y multiplicacion de los dos numeros ingresados
+	cout << "Suma de los numeros: " << sumar() << endl;
+	cout << "Multiplicacion de los numeros: " << multiplicar() << endl;
 	
 	return 0;
 }
